fe656a6b12ac12e6a1ad83719d6cfe18bac9d6fa
Jernej Kavka (JK) <jernejkavka@ssw.com.au>
2018-05-14 23:27:11 +1000

    Upgraded to .Net Core 2.1

--- Presentation/Startup.cs
+++ Presentation/Startup.cs
@@ -1,28 +1,23 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Autofac;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
-using NorthwindTraders.Persistance;
-using Autofac;
 using NorthwindTraders.Infrastructure;
+using NorthwindTraders.Persistance;
 
 namespace NorthwindTraders
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            Configuration = configuration;
         }
 
-        public IConfigurationRoot Configuration { get; }
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
