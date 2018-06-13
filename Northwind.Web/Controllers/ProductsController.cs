2dc524f9a4596314b03a5e76bedaf1787df6a31b
Jason Taylor <jasongt@outlook.com>
2018-06-13 11:53:48 +1000

    Update Products. Switch Controller to use FromServices.

--- Northwind.Web/Controllers/ProductsController.cs
+++ Northwind.Web/Controllers/ProductsController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 using Northwind.Application.Products.Commands;
 using Northwind.Application.Products.Models;
 using Northwind.Application.Products.Queries;
-using Northwind.Persistence;
 
 namespace Northwind.Web.Controllers
 {
@@ -12,61 +11,50 @@ namespace Northwind.Web.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        private readonly IGetAllProductsQuery _getAllProductsQuery;
-        private readonly IGetProductQuery _getProductQuery;
-        private readonly ICreateProductCommand _createProductCommand;
-        private readonly IUpdateProductCommand _updateProductCommand;
-        private readonly IDeleteProductCommand _deleteProductCommand;
-
-        public ProductsController(NorthwindDbContext context,
-            IGetAllProductsQuery getAllProductsQuery,
-            IGetProductQuery getProductQuery,
-            ICreateProductCommand createProductCommand,
-            IUpdateProductCommand updateProductCommand,
-            IDeleteProductCommand deleteProductCommand)
-        {
-            _getAllProductsQuery = getAllProductsQuery;
-            _getProductQuery = getProductQuery;
-            _createProductCommand = createProductCommand;
-            _updateProductCommand = updateProductCommand;
-            _deleteProductCommand = deleteProductCommand;
-        }
-
         // GET: api/Products
         [HttpGet]
-        public async Task<IEnumerable<ProductDto>> GetProducts()
+        public async Task<IEnumerable<ProductDto>> GetProducts(
+            [FromServices] IGetAllProductsQuery query)
         {
-            return await _getAllProductsQuery.Execute();
+            return await query.Execute();
         }
 
         // GET: api/Products/5
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetProduct([FromRoute] int id)
+        public async Task<IActionResult> GetProduct(
+            [FromServices] IGetProductQuery query,
+            int id)
         {
-            return Ok(await _getProductQuery.Execute(id));
+            return Ok(await query.Execute(id));
         }
 
         // POST: api/Products
         [HttpPost]
-        public async Task<IActionResult> PostProduct([FromBody] ProductDto product)
+        public async Task<IActionResult> PostProduct(
+            [FromServices] ICreateProductCommand command,
+            [FromBody] ProductDto product)
         {
-            var newProduct = await _createProductCommand.Execute(product);
+            var newProduct = await command.Execute(product);
 
             return CreatedAtAction("GetProduct", new { id = newProduct.ProductId }, newProduct);
         }
 
         // PUT: api/Products/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutProduct([FromBody] ProductDto product)
+        public async Task<IActionResult> PutProduct(
+            [FromServices] IUpdateProductCommand command,
+            [FromBody] ProductDto product)
         {
-            return Ok(await _updateProductCommand.Execute(product));
+            return Ok(await command.Execute(product));
         }
 
         // DELETE: api/Products/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteProduct([FromRoute] int id)
+        public async Task<IActionResult> DeleteProduct(
+            [FromServices] IDeleteProductCommand command,
+            int id)
         {
-            await _deleteProductCommand.Execute(id);
+            await command.Execute(id);
 
             return NoContent();
         }
