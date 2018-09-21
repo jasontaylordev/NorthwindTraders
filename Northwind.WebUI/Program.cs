6ba856fd98677d7bb2884881fb49a2a68847b3d2
smiley <teamsmiley@gmail.com>
2018-09-20 19:08:05 -0700

    db migration best practice

--- Northwind.WebUI/Program.cs
+++ Northwind.WebUI/Program.cs
@@ -1,17 +1,39 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Northwind.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Northwind.WebUI
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+      var host = CreateWebHostBuilder(args).Build();
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+      // migrate the database.  Best practice = in Main, using service scope
+      using (var scope = host.Services.CreateScope())
+      {
+        try
+        {
+          var context = scope.ServiceProvider.GetService<NorthwindDbContext>();
+          context.Database.Migrate();
+        }
+        catch (Exception ex)
+        {
+          var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+          logger.LogError(ex, "An error occurred while migrating the database.");
+        }
+      }
+
+      // run the web app
+      host.Run();
     }
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>();
+  }
 }
