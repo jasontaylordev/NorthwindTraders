c029d6b53219823833201e5e7a1f08d18935cd1f
Jason Taylor <jasongt@outlook.com>
2019-03-24 07:51:54 +1000

    #72 - Added unit tests for ValueObject.

---
+++ Northwind.Domain.Tests/Infrastructure/ValueObjectTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Northwind.Domain.Infrastructure;
+using Xunit;
+
+namespace Northwind.Domain.Tests.Infrastructure
+{
+    public class ValueObjectTests
+    {
+        [Fact]
+        public void Equals_GivenDifferentValues_ShouldReturnFalse()
+        {
+            var point1 = new Point(1, 2);
+            var point2 = new Point(2, 1);
+
+            Assert.False(point1.Equals(point2));
+        }
+
+        [Fact]
+        public void Equals_GivenMatchingValues_ShouldReturnTrue()
+        {
+            var point1 = new Point(1, 2);
+            var point2 = new Point(1, 2);
+
+            Assert.True(point1.Equals(point2));
+        }
+
+        private class Point : ValueObject
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            private Point() { }
+
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            protected override IEnumerable<object> GetAtomicValues()
+            {
+                yield return X;
+                yield return Y;
+            }
+        }
+    }
+}
