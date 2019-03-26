e904bee96f1158da89db4bfa2101b5cc2138e8e2
Jason Taylor <jasongt@outlook.com>
2019-03-26 14:37:27 +1000

    #93 - Functional tests for products controller.

---
+++ Northwind.WebUI.FunctionalTests/Common/CustomWebApplicationFactory.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Northwind.Persistence;
+
+namespace Northwind.WebUI.FunctionalTests.Common
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // Create a new service provider.
+                var serviceProvider = new ServiceCollection()
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+
+                // Add a database context using an in-memory 
+                // database for testing.
+                services.AddDbContext<NorthwindDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                    options.UseInternalServiceProvider(serviceProvider);
+                });
+
+                // Build the service provider.
+                var sp = services.BuildServiceProvider();
+
+                // Create a scope to obtain a reference to the database
+                // context (NorthwindDbContext)
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var context = scopedServices.GetRequiredService<NorthwindDbContext>();
+                    var logger = scopedServices
+                        .GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                    // Ensure the database is created.
+                    context.Database.EnsureCreated();
+
+                    try
+                    {
+                        // Seed the database with test data.
+                        Utilities.InitializeDbForTests(context);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, $"An error occurred seeding the " +
+                                            "database with test messages. Error: {ex.Message}");
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ Northwind.WebUI.FunctionalTests/Common/Utilities.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.Persistence;
+
+namespace Northwind.WebUI.FunctionalTests.Common
+{
+    public class Utilities
+    { 
+        public static StringContent GetRequestContent(object obj)
+        {
+            return new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+        }
+
+        public static async Task<T> GetResponseContent<T>(HttpResponseMessage response)
+        {
+            var stringResponse = await response.Content.ReadAsStringAsync();
+
+            var result = JsonConvert.DeserializeObject<T>(stringResponse);
+
+            return result;
+        }
+
+        public static void InitializeDbForTests(NorthwindDbContext context)
+        {
+            NorthwindInitializer.Initialize(context);
+        }
+    }
+}
