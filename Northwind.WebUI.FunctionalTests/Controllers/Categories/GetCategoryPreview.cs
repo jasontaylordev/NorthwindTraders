88d3764864258fdf54c2e21898b9efae409035e2
Jason Taylor <jasongt@outlook.com>
2019-03-26 18:01:40 +1000

    #93 - Functional tests for categories controller

---
+++ Northwind.WebUI.FunctionalTests/Controllers/Categories/GetCategoryPreview.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Northwind.Application.Categories.Models;
+using Northwind.WebUI.FunctionalTests.Common;
+using Xunit;
+
+namespace Northwind.WebUI.FunctionalTests.Controllers.Categories
+{
+    public class GetCategoryPreview : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _client;
+
+        public GetCategoryPreview(CustomWebApplicationFactory<Startup> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task ReturnsIEnumerableOfCategoryPreviewDto()
+        {
+            var response = await _client.GetAsync("/api/categories/getcategorypreview");
+
+            response.EnsureSuccessStatusCode();
+
+            var categories = await Utilities.GetResponseContent<IEnumerable<CategoryPreviewDto>>(response);
+
+            Assert.NotEmpty(categories);
+        }
+    }
+}
