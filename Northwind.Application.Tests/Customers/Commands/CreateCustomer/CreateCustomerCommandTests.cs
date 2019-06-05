8eba55f7b98f82169679dca27ea2724415200588
Jason Taylor <jasongt@outlook.com>
2019-06-05 21:06:52 +1000

    #102 Example unit test for to ensure customer created event for correct customer.

---
+++ Northwind.Application.Tests/Customers/Commands/CreateCustomer/CreateCustomerCommandTests.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Moq;
+using Northwind.Application.Customers.Commands.CreateCustomer;
+using Northwind.Application.Tests.Infrastructure;
+using System.Threading;
+using Xunit;
+
+namespace Northwind.Application.Tests.Customers.Commands.CreateCustomer
+{
+    public class CreateCustomerCommandTests : CommandTestBase
+    {
+        [Fact]
+        public void Handle_GivenValidRequest_ShouldRaiseCustomerCreatedNotification()
+        {
+            // Arrange
+            var mediatorMock = new Mock<IMediator>();
+            var sut = new CreateCustomerCommand.Handler(_context, mediatorMock.Object);
+            var newCustomerId = "QAZQ1";
+
+            // Act
+            var result = sut.Handle(new CreateCustomerCommand { Id = newCustomerId }, CancellationToken.None);
+
+
+            // Assert
+            mediatorMock.Verify(m => m.Publish(It.Is<CustomerCreated>(cc => cc.CustomerId == newCustomerId), It.IsAny<CancellationToken>()), Times.Once);
+        }
+    }
+}
