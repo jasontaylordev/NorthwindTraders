dbd66fd233c592cc1c6a740193b7f9c8f32d9777
Havaux Valérian <valerian.havaux@outlook.com>
2019-06-05 12:53:30 +0200

    Feature/client app add customer detail (#114)
    Added customer detail modal to customers list.

--- Northwind.WebUI/ClientApp/src/app/app.module.ts
+++ Northwind.WebUI/ClientApp/src/app/app.module.ts
@@ -1,22 +1,47 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './/app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from "./app.component";
-import { NavSideMenuComponent } from "./nav-side-menu/nav-side-menu.component";
-import { NavTopMenuComponent } from "./nav-top-menu/nav-top-menu.component";
-import { HomeComponent } from "./home/home.component";
-import { CustomersComponent } from "./customers/customers.component";
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { CustomersComponent } from './customers/customers.component';
+import { NavTopMenuComponent } from './nav-top-menu/nav-top-menu.component';
+import { NavSideMenuComponent } from './nav-side-menu/nav-side-menu.component';
+import { CustomerDetailComponent } from './customer-detail/customer-detail.component';
 
-import { CustomersClient, ProductsClient } from "./northwind-traders-api";
-import { AppRoutingModule } from ".//app-routing.module";
-import { ProductsComponent } from "./products/products.component";
+import { CustomersClient, ProductsClient } from './northwind-traders-api';
+
+import { CamelCaseToText } from '../pipes/camel-case-to-text';
+
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+
+const modules = [
+  BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+  HttpClientModule,
+  FormsModule,
+  AppRoutingModule,
+  ModalModule.forRoot()
+];
+const components = [
+  AppComponent,
+  NavTopMenuComponent,
+  NavSideMenuComponent,
+  HomeComponent,
+  ProductsComponent,
+  CustomersComponent,
+  CustomerDetailComponent
+];
+const pipes = [CamelCaseToText];
 
 @NgModule({
-  declarations: [AppComponent, NavTopMenuComponent, NavSideMenuComponent, HomeComponent, CustomersComponent, ProductsComponent],
-  imports: [BrowserModule.withServerTransition({ appId: "ng-cli-universal" }), HttpClientModule, FormsModule, AppRoutingModule],
+  declarations: [...components, ...pipes],
+  imports: [...modules],
   providers: [CustomersClient, ProductsClient],
+  entryComponents: [CustomerDetailComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ Northwind.WebUI/ClientApp/src/app/customer-detail/customer-detail.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { CustomerDetailModel } from '../northwind-traders-api';
+
+@Component({
+  selector: 'app-customer-detail',
+  templateUrl: './customer-detail.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CustomerDetailComponent implements OnInit {
+  public customer: CustomerDetailModel;
+  public detailKeys: string[] = [];
+
+  constructor(private bsModalRef: BsModalRef) {}
+
+  ngOnInit() {
+    this.detailKeys = Object
+      .keys(this.customer)
+      .filter(keys => keys !== 'id');
+  }
+
+  public closeModal() {
+    this.bsModalRef.hide();
+  }
+
+}
