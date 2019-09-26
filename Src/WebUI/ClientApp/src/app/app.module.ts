ed69591ff772b5f27323ffde8a3c261ec16c1877
Jason Taylor <jasongt@outlook.com>
2019-09-26 22:55:50 +1000

    Configure base API urls by environment.

--- Src/WebUI/ClientApp/src/app/app.module.ts
+++ Src/WebUI/ClientApp/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { NavTopMenuComponent } from './nav-top-menu/nav-top-menu.component';
 import { NavSideMenuComponent } from './nav-side-menu/nav-side-menu.component';
 import { CustomerDetailComponent } from './customer-detail/customer-detail.component';
 
-import { CustomersClient, ProductsClient } from './northwind-traders-api';
+import { CustomersClient, ProductsClient, API_BASE_URL } from './northwind-traders-api';
 
 import { CamelCaseToText } from '../pipes/camel-case-to-text';
 
@@ -20,6 +20,7 @@ import { ModalModule } from 'ngx-bootstrap/modal';
 import { AppIconsModule } from './app.icons.module';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
+import { environment } from "../environments/environment";
 
 @NgModule({
   declarations: [
@@ -43,6 +44,7 @@ import { AuthorizeInterceptor } from 'src/api-authorization/authorize.intercepto
   ],
   providers: [
       { provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true },
+      { provide: API_BASE_URL, useValue: environment.apiBaseUrl },
       CustomersClient,
       ProductsClient
   ],
--- Src/WebUI/ClientApp/src/environments/environment.prod.ts
+++ Src/WebUI/ClientApp/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+    production: true,
+    apiBaseUrl: "https://northwindtraders.azurewebsites.net"
 };
