e69da596e440b09d036a26e93671eaaa17aca734
Rick Su <email@rick.su>
2019-10-25 15:05:44 +1100

    Fixed MappingProfile so that domain entities can - implement multiple `IMapFrom<T>` - Or, use default interface implementation

--- Src/Application/Common/Mappings/MappingProfile.cs
+++ Src/Application/Common/Mappings/MappingProfile.cs
@@ -14,16 +14,31 @@ namespace Northwind.Application.Common.Mappings
 
         private void ApplyMappingsFromAssembly(Assembly assembly)
         {
-            var types = assembly.GetExportedTypes()
-                .Where(t => t.GetInterfaces().Any(i => 
-                    i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapFrom<>)))
-                .ToList();
-
+            var types = assembly.GetExportedTypes();
+            var interfaceType = typeof(IMapFrom<>);
+            var methodName = nameof(IMapFrom<object>.Mapping);
+            var argumentTypes = new Type[] { typeof(Profile) };
+            
             foreach (var type in types)
             {
-                var instance = Activator.CreateInstance(type);
-                var methodInfo = type.GetMethod("Mapping");
-                methodInfo?.Invoke(instance, new object[] { this });
+                var interfaces = type.GetInterfaces()
+                    .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == interfaceType)
+                    .ToList();
+
+                // Has the type implemented any IMapFrom<T>?
+                if (interfaces.Count > 0)
+                {
+                    // Yes, then let's create an instance
+                    var instance = Activator.CreateInstance(type);
+
+                    // and invoke each implementation of `.Mapping()`
+                    foreach (var i in interfaces)
+                    {
+                        var methodInfo = i.GetMethod(methodName, argumentTypes);
+
+                        methodInfo?.Invoke(instance, new object[] { this });
+                    }
+                }
             }
         }
     }
